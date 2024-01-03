ebae93ac05434e3981e66ff8d410e8cd1e28b585
beautiful1112 <winecrazy1112@gmail.com>
2024-01-03 16:30:15 +0800

    change

--- huawei.py
+++ huawei.py
@@ -3,7 +3,7 @@ from connection import Net
 
 class VRP8(Net):
     def __init__(self):
-        super().__init__('huawei', '192.168.229.10', 'huawei', 'Yestar@123')
+        super().__init__('huawei_vrpv8', '192.168.229.10', 'huawei', 'Yestar@123')
 
     # 获取配置
     ## 输出字符串形式的设备配置
@@ -18,20 +18,21 @@ class VRP8(Net):
         cmd = 'display ip interface brief'
         info = self.device.send_command(cmd)
         data_list = []
-        for line in info.split('\n')[1:]:
+        for line in info.splitlines()[11:]:
             line_list = line.split()
             if_name = line_list[0]
             if_ip = line_list[1]
-            status = line_list[-1]
+            status = line_list[2]
+            protocol = line_list[3]
             data_list.append({'name': if_name,
                               'ip': if_ip,
-                              'status': status})
+                              ' Physical': status,
+                              'Protocol':protocol})
         return data_list
-
         # 接口恢复UP
     def recover_interface(self, if_name):
-        self.reconnect()
-        cmd_list = [f'interface {if_name}', 'no shutdown']
+        #self.reconnect()
+        cmd_list = [f'interface {if_name}', 'undo shutdown', 'commit']
         self.device.send_config_set(cmd_list)
 
     # 获取路由表
@@ -45,15 +46,15 @@ class VRP8(Net):
     # 新增路由条目
     def post_route(self, dst_n, mask, next):
         self.reconnect()
-        cmd_list = [f'ip route static {dst_n} {mask} {next}']
+        cmd_list = [f'ip route-static {dst_n} {mask} {next}', 'commit']
         self.device.send_config_set(cmd_list)
 
     # 自动化巡检
     ## 输出字典形式的CPU使用率数据
     def monitor(self):
-        cpu_cmd = 'show processes cpu'
+        cpu_cmd = 'display cpu'
         cpu_info = self.device.send_command(cpu_cmd)
-        line = cpu_info.split('\n')[0].split('five seconds: ')[1]
+        line = cpu_info.split('\n')[0].split('MaxTime')[1]
         cpu_list = line.split('%')
         data_dict = {'cpu1_5s': cpu_list[0][-1:],
                      'cpu2_5s': cpu_list[1][-1:],
