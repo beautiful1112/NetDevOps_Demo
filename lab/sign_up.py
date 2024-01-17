3473cab6e38409610135e3352abd787e26a4f347
beautiful1112 <winecrazy1112@gmail.com>
2024-01-17 11:31:04 +0800

    change

--- lab/sign_up.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def register(database, username, password, level):
-    user_id = str(random.randint(1,100))
-    database[user_id] = {'username': username,
-                         'password': password,
-                         'level': level}
-    print(f'User {username} has already been record, User ID is {user_id}')
-    return database, user_id
