b87d4062a30918ad63462bf8ae8ce11b4c7c8a71
beautiful1112 <winecrazy1112@gmail.com>
2024-03-28 17:07:06 +0800

    change

--- fastAPI/gei_device_name.py
+++ fastAPI/gei_device_name.py
@@ -4,9 +4,9 @@ import uvicorn
 app = FastAPI()
 
 
-@app.get("/")
-async def hello():
-    return {"DeviceOS_Version": "Cisco IOSXE 17.01"}
+@app.get("/device/{deviceInfo}")
+async def hello(deviceInfo : str):
+    return {f"DeviceOS_Version": f"{deviceInfo}"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5000, log_level="info")
